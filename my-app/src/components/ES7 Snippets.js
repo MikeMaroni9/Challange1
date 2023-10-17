e6934b0c954de42902dbf3bc90e823ba96551286
MikeMaroni9 <>
2023-10-17 20:25:40 +0000

    es snipepts

---
+++ my-app/src/components/ES7 Snippets.js
@@ -0,0 +1,63 @@
+rfce
+import React from 'react'
+
+function ES7 Snippets() {
+  return (
+    <div>ES7 Snippets</div>
+  )
+}
+
+export default ES7 Snippets
+
+
+
+rafce
+import React from 'react'
+rafce
+const ES7 Snippets = () => {
+  return (
+    <div>ES7 Snippets</div>
+  )rafce
+}
+
+export default ES7 Snippets
+
+
+
+
+
+rce
+import React, { Component } from 'react'
+
+export clas
+rces ES7 Snippets extends Component {
+  render() {
+    return (
+      <div>ES7 Snippets</div>
+    )
+  }
+  rce
+}
+
+export default ES7 Snippets
+
+
+
+rconst
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+
+
+clg
+console.log(first)
+
+
+
+clo
+console.log('first', first)
